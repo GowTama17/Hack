d4c4e2c0968dcee7ae364acdcdeb37b24f0ac244
percivale17 <78723149+percivale17@users.noreply.github.com>
2022-02-08 13:13:10 +0300

    save

--- Hack/db_a7d8d7_testingContext.cs
+++ Hack/db_a7d8d7_testingContext.cs
@@ -27,7 +27,7 @@ namespace Hack
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=SQL5105.site4now.net,1433;Initial Catalog=db_a7d8d7_testing;User Id=db_a7d8d7_testing_admin;Password=123321Ok");
+                optionsBuilder.UseSqlServer("******************************************************************************************");
             }
         }
 
